9c94ae0f0481baaa0032fb76654002e1ded2da57
Eckschlager Simon <simoneckschlager2.0@gmail.com>
2024-04-26 11:46:05 +0200

    added player kill rewards

--- src/main/java/org/zimbls/DieLigaDerKeks/listener/PlayerDeathListener.java
+++ src/main/java/org/zimbls/DieLigaDerKeks/listener/PlayerDeathListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.zimbls.DieLigaDerKeks.game.Participant;
 import org.zimbls.DieLigaDerKeks.stateMachine.GameState;
 import org.zimbls.DieLigaDerKeks.stateMachine.GameStateMachine;
+import org.zimbls.DieLigaDerKeks.util.Mob;
 
 public class PlayerDeathListener implements Listener {
    private GameStateMachine state;
@@ -23,6 +24,14 @@ public class PlayerDeathListener implements Listener {
       if (state.getState() == GameState.RUNNING && participant != null) { // Check if the game is running
          player.setGameMode(GameMode.SPECTATOR); // Set the player's game mode to Spectator
          participant.setDead(true);
+         if (player.getKiller() != null) {
+            Participant killer = state.getGame().getParticipantByName(player.getKiller().getName());
+            if (killer != null) {
+               Mob mob = state.getGame().getMobFormMobName("Player");
+               killer.addIfNewMob(mob);
+               player.getWorld().dropItemNaturally(player.getLocation(), mob.getReward());
+            }
+         }
       }
    }
 }
