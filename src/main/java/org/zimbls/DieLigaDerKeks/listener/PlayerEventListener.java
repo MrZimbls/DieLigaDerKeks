5a70d270a37243f774a74cd3ae7aa20421401ad8
Eckschlager Simon <simoneckschlager2.0@gmail.com>
2024-05-02 22:42:46 +0200

    fix player is now detected as participant

--- src/main/java/org/zimbls/DieLigaDerKeks/listener/PlayerEventListener.java
+++ src/main/java/org/zimbls/DieLigaDerKeks/listener/PlayerEventListener.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.zimbls.DieLigaDerKeks.game.Participant;
 import org.zimbls.DieLigaDerKeks.stateMachine.GameState;
 import org.zimbls.DieLigaDerKeks.stateMachine.GameStateMachine;
 
@@ -20,10 +21,12 @@ public class PlayerEventListener implements Listener {
    public void onPlayerJoin(PlayerJoinEvent event) {
       Player player = event.getPlayer();
       if (state.getState() != GameState.STOPPED) {
-         if (state.getGame().getParticipatingPlayers().contains(player)) {
+         Participant participant = state.getGame().getParticipantByName(player.getName());
+         if (participant != null) {
             player.sendMessage(ChatColor.GOLD + "Welcome back!");
             if (state.getState() == GameState.RUNNING) {
-               state.getGame().getParticipantByName(player.getName()).teleportToGameMap(state.getGame().getGameWorld());
+               participant.teleportToGameMap(state.getGame().getGameWorld());
+               participant.setScoreboard();
             } else if (state.getState() == GameState.EVENT || state.getState() == GameState.PAUSED) {
                state.getGame().getParticipantByName(player.getName()).teleportToLobbyMap(state.getGame().getLobbyMap());
             }
@@ -33,9 +36,10 @@ public class PlayerEventListener implements Listener {
 
    @EventHandler
    public void onPlayerQuit(PlayerQuitEvent event) {
+      System.out.println("PlayerQuitEvent triggered!");
       Player player = event.getPlayer();
       if (state.getState() == GameState.RUNNING) {
-         if (state.getGame().getParticipatingPlayers().contains(player)) {
+         if (state.getGame().getParticipantByName(player.getName()) != null) {
             state.getGame().getParticipantByName(player.getName()).setLastGameLocation();
          }
       }
