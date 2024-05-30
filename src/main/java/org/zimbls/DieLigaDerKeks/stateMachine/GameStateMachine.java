b8ea2c336cbfc20ac16a7fb1858479c3d433c9f0
stefanfeldner <stefanfeldner@gmail.com>
2024-05-30 13:27:45 +0200

    refactor: Readd available events

--- src/main/java/org/zimbls/DieLigaDerKeks/stateMachine/GameStateMachine.java
+++ src/main/java/org/zimbls/DieLigaDerKeks/stateMachine/GameStateMachine.java
@@ -40,13 +40,13 @@ public class GameStateMachine {
         currentState = GameState.STARTING;
         game = new Game(lobbyMap, plugin, this);
         game.createGameWorld();
-//        availableEvents.add(new RandomPlayerTpEvent(this));
-//        availableEvents.add(new SwapPointsEvent(this));
-//        availableEvents.add(new RevealPointsEvent(this));
-//        availableEvents.add(new HalfWorldBorderEvent(this));
-//        availableEvents.add(new RevealLocationEvent(this));
-//        availableEvents.add(new BuffLastTwoPlayersEvent(this));
-//        availableEvents.add(new NerfTopTwoPlayersEvent(this));
+        availableEvents.add(new RandomPlayerTpEvent(this));
+        availableEvents.add(new SwapPointsEvent(this));
+        availableEvents.add(new RevealPointsEvent(this));
+        availableEvents.add(new HalfWorldBorderEvent(this));
+        availableEvents.add(new RevealLocationEvent(this));
+        availableEvents.add(new BuffLastTwoPlayersEvent(this));
+        availableEvents.add(new NerfTopTwoPlayersEvent(this));
         availableEvents.add(new FirstToSleepInBedEvent(this));
         this.plugin = plugin;
 
